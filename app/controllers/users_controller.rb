fb656dd5042061291cad8405ab460f2b1f323a6e
Scot <scothw@gmail.com>
2013-12-26 23:41:54 -0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params) # Not the final implementation!
   	if @user.save
+      sign_in @user
   		flash[:success] = "Welcome to the Microblog App!"
   		redirect_to @user
   	else
